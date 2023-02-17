22557e47f11df80a0633e2432ddd3a7c5cd2eecb
Mohamed Dhibi <92624988+MohamedDhibi@users.noreply.github.com>
2023-02-17 10:58:55 +0100

    initial commit

---
+++ AM.ApplicationCore/CoreExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore
+{
+    public static class CoreExtension
+    {
+        public static void ShowList<T>(this IEnumerable<T> list, string title, ShowLine showLine)
+        {
+            showLine(title);
+
+            foreach (var item in list)
+            {
+                showLine(item);
+            }
+        }
+    }
+}
---
+++ AM.ApplicationCore/Interfaces/IFlightService.cs
@@ -0,0 +1,16 @@
+﻿using AM.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public interface IFlightService : IService<Flight>
+    {
+        void ShowFlights(string filterType, string filterValue);
+        IEnumerable<Flight> GetFlightsByDestination(string destination);
+        IEnumerable<Flight> GetFlightsGreaterThan(DateTime flightDate);
+    }
+}
